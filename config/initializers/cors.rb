44c01be5c52ec7dfc4dc4c3549ded5592ff71cdd
Derrick Foster <derrick.j.foster@gmail.com>
2019-08-17 10:17:57 -0600

    changed cors whitelist from 3000/3001 to heroku urls

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://champ-heroku-api.herokuapp.com, https://champ-heroku-frontend.herokuapp.com'
+    origins 'https://champ-heroku-api.herokuapp.com', 'https://champ-heroku-frontend.herokuapp.com', 'localhost:3001', 'localhost:3000'
 
     resource '*',
       headers: :any,
